b65033768a995529159c5da98ba737c46f6180f3
vssabev <vsabev@lirex.com>
2025-01-04 18:45:29 +0200

    Added register Logout going to the login page

---
+++ IRSv2.0/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using IRSv2._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace IRSv2._0.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model) {
+            if(model.Id == "aa")
+            {
+            return RedirectToAction("Orders", "Orders");
+
+            }
+            else if(model.Id == "bb")
+            {
+                return RedirectToAction("ToGoOrders", "ToGoOrders");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ IRSv2.0/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IRSv2._0.Models
+{
+    public class RegisterModel
+    {
+        public string Id { get; set; }
+    }
+}
