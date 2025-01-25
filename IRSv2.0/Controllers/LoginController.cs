ba76b4e43bc5b5eec505d5256111ae3594906057
vssabev <vsabev@lirex.com>
2025-01-25 14:27:18 +0200

    Почистване на излишен код

--- IRSv2.0/Controllers/LoginController.cs
+++ IRSv2.0/Controllers/LoginController.cs
@@ -1,57 +1,4 @@
-﻿/*using IRSv2._0.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace IRSv2._0.Controllers
-{
-    public class LoginController : Controller
-    {
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(LoginModel model)
-        {
-            RedirectToAction("Orders", "Orders");
-            char positionLetter = char.ToUpper(model.Id.ToString()[0]);
-            int idNumber = int.Parse(model.Id.Substring(1));
-
-             if (!ModelState.IsValid)
-             {
-                 // Ако моделът не е валиден, върни същото View с грешки
-                 return View(model);
-             }
-
-             switch (positionLetter)
-             {
-                 case 'M':
-                     RedirectToAction("Orders", "Orders");
-                     break;
-                 case 'S':
-                     RedirectToAction("Orders", "Orders");
-                     break;
-                 case 'C':
-                     RedirectToAction("Orders", "Orders");
-                     break;
-                 case 'H':
-                     RedirectToAction("Orders", "Orders");
-                     break;
-                 case 'D':
-                     RedirectToAction("Orders", "Orders");
-                     break;
-             }
-            
-             // За демонстрация - съобщение за успешна регистрация
-             Console.WriteLine( "User {0} logged successfully!", model.Id);
-
-            return View();
-        }
-    }
-}
-*/
-using IRSv2._0.Models;
+﻿using IRSv2._0.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IRSv2._0.Controllers
@@ -108,14 +55,13 @@ namespace IRSv2._0.Controllers
             switch (positionLetter)
             {
                 case 'M':
-                case 'S':
                 case 'C':
                 case 'H':
                 case 'D':
                 case 'W':
                     return RedirectToAction("Orders", "Orders");
                 default:
-                    ModelState.AddModelError("Id", "Invalid ID. The first letter must be W, M, S, C, H, or D.");
+                    ModelState.AddModelError("Id", "Invalid ID. The first letter must be W, M, C, H, or D.");
                     return View(model);
             }
 
